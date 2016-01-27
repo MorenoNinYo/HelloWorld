901ee76e10f040c3491b79fb4c37d3c9d908fa4f
lucahernandezacosta <l.hernandezacosta@stud.uni-goettingen.de>
2016-01-27 18:10:01 +0100

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Luca Hernández Acosta on 27.01.16.
+//  Copyright © 2016 Luca Hernández Acosta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
